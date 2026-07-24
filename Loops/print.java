c09f954cdd3ebb26668cf64234d7b7eb77cf1d5b
Rajan <rajanjadhao420@gmail.com>
2026-07-24 11:26:16 +0530

    Moved print.java into Loops

---
+++ Loops/print.java
@@ -0,0 +1,16 @@
+package Loops;
+//PRINT NUMBERS FROM 1 TO N
+
+import java.util.*;
+public class print {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int counter = 1;
+        while(counter <= n){
+            System.out.print(counter + " ");
+            counter++;
+        }
+        System.out.println();
+    }
+}
