456f797ab14cc73f155b8d5c84a06acbe17cba65
Rajan <rajan071023@gmail.com>
2026-07-27 13:46:17 +0530

    Solved patter using nested loop

---
+++ Loops/pattern.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class pattern {
+    
+    public static void main(String args[]){
+
+    for(int line = 1; line <= 5; line ++){
+    for(int star =1; star <= line; star ++){
+        System.out.print("*");
+    }
+        System.out.println();
+    }
+    }
+}
