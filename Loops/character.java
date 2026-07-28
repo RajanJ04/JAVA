ef8d9e900e8fff6efee4dfdcca8c66e60851d390
Rajan <rajan071023@gmail.com>
2026-07-28 16:38:23 +0530

    Solving character pattern

---
+++ Loops/character.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class character {
+    public static void main(String args[]){
+        char ch = 'A';
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int chars = 1; chars <= i; chars++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
