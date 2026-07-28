35ff7ff3073a528c860baa8e044db0c7a6d3640b
Rajan <rajan071023@gmail.com>
2026-07-28 20:14:33 +0530

    Learning functions/methods

---
+++ Functions/syntax.java
@@ -0,0 +1,9 @@
+public class syntax{
+    public static void printhelloworld(){
+        System.out.println("Hello world");
+        return;
+    }
+    public static void main(String args[]){
+        printhelloworld();
+    }
+}
