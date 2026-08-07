4b42e9f072872e7127ffbad578b49c5efd919262
Rajan <rajan071023@gmail.com>
2026-08-07 18:53:43 +0530

    Solved basic finding largest element

---
+++ Arrays/largest.java
@@ -0,0 +1,16 @@
+public class largest {
+    public static int Getlargest(int number[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<number.length; i++){
+            if(largest < number[i]){
+                largest = number[i];
+            }
+        }
+        return largest;
+    }
+public static void main(String args[]){
+
+        int number[] = {5, 3, 8, 7, 2};
+        System.out.println("The largest value in the array is : " + Getlargest(number));
+}
+}
