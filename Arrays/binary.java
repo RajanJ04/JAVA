f471bb3138baa7cb8984093835cf872b9927c313
Rajan <rajan071023@gmail.com>
2026-08-07 19:22:46 +0530

    Binary search

---
+++ Arrays/binary.java
@@ -0,0 +1,29 @@
+package Arrays;
+public class binary {
+
+    public static int binarysearch(int number[], int key){
+        int start =0;
+        int end = number.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+
+            if(number[mid] == key){
+                return mid;
+            }
+            if(number[mid] < key){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            }
+            return -1;
+        }
+        public static void main(String args[]){
+
+        int number[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int key = 60;
+        System.out.println("The key is present at index: " + binarysearch(number, key));
+    }
+}
+    
+
